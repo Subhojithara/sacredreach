5557a1350a5bafa2c072b63e6d66fcce9e638a7a
subhajit hara <harasubhojit@gmail.com>
2024-08-29 23:41:49 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Loader />
         <Navbar />
         {children}
         <Footer />
